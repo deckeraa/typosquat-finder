bb7b4bc823d4a36b7e410b595b83527904926484
Aaron Decker <decker49@gmail.com>
2020-10-02 20:31:54 -0500

    Add the list of pulled projects.

--- src/typosquat_finder/core.clj
+++ src/typosquat_finder/core.clj
@@ -1,5 +1,6 @@
 (ns typosquat-finder.core
-  (:require [net.cgrand.enlive-html :as html]))
+  (:require [net.cgrand.enlive-html :as html]
+            [clj-fuzzy.metrics :as metrics]))
 
 (defn get-projects
   ([page-number]
@@ -8,12 +9,33 @@
      (map :content (html/select % [:div.result :> [(html/nth-of-type 2) :a]]))
      (flatten %)))
   ([]
-   (apply concat (mapv get-projects (range 1 3)))))
+   (apply concat (mapv get-projects (range 1 1352)))))
+
+(defn get-levenshtein-neighbors [word word-list max-edit-distance]
+  (as-> word-list %
+    (map (fn [word-two]
+           [(metrics/levenshtein word word-two)
+            word-two])
+         %)
+    (remove (fn [[distance word]]
+              (or (> distance max-edit-distance)
+                  (= 0 distance) ;; take out duplicates
+                  )) %)))
+
+(defn find-neighboring-projects [project-name-list]
+  (as-> project-name-list %
+    (mapv
+     (fn [project] [project (get-levenshtein-neighbors project project-name-list 2)])
+     %)
+    (remove (fn [[project neighbor-list]] (empty? neighbor-list)) %)))
 
 ;; (html/select foo [:div.result [(html/nth-of-type 2) :a]])
 ;; (flatten (map :content (html/select foo [:div.result :> [(html/nth-of-type 2) :a]])))
 
-(defonce foo (get-page))
 
 
+;; (def foo (vec (get-projects 1352)))
 
+;; projects_list.end is a full pull from Clojars on 10/2/2020.
+;; (def foo (clojure.edn/read-string (clojure.string/replace (slurp "projects_list.edn") #"\]\[" "")))
+;; (def foo (clojure.edn/read-string (clojure.string/replace (slurp "a_projects.edn") #"\]\[" "")))
